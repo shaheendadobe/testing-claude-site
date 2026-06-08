1180e9156406e78949348e661b74c3280c6133fc
Shaheen Danho <sdanho@adobe.com>
2026-06-08 14:32:06 -0400

    linting

--- blocks/track/track.js
+++ blocks/track/track.js
@@ -1,3 +1,3 @@
-export default function decorate(block) {
+export default function decorate() {
   // No JS needed — pure CSS block
 }
---
+++ scripts/placeholders.js
@@ -0,0 +1,38 @@
+import { toCamelCase } from './aem.js';
+
+/**
+ * Gets placeholders object.
+ * @param {string} [prefix] Location of placeholders
+ * @returns {object} Window placeholders object
+ */
+// eslint-disable-next-line import/prefer-default-export
+export async function fetchPlaceholders(prefix = 'default') {
+  window.placeholders = window.placeholders || {};
+  if (!window.placeholders[prefix]) {
+    window.placeholders[prefix] = new Promise((resolve) => {
+      fetch(`${prefix === 'default' ? '' : prefix}/placeholders.json`)
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.json();
+          }
+          return {};
+        })
+        .then((json) => {
+          const loadedPlaceholders = {};
+          json.data
+            .filter((placeholder) => placeholder.Key)
+            .forEach((placeholder) => {
+              loadedPlaceholders[toCamelCase(placeholder.Key)] = placeholder.Text;
+            });
+          window.placeholders[prefix] = loadedPlaceholders;
+          resolve(window.placeholders[prefix]);
+        })
+        .catch(() => {
+          // error loading placeholders
+          window.placeholders[prefix] = {};
+          resolve(window.placeholders[prefix]);
+        });
+    });
+  }
+  return window.placeholders[prefix];
+}
